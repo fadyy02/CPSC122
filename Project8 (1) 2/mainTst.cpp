c106cdd40624200434a560a10a31c06afe065900
Fady Youssef <Thefady4@gmail.com>
2024-03-28 15:59:00 -0700

    Add files via upload

---
+++ Project8 (1) 2/mainTst.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "main.h"
+
+int main()
+{
+
+ ListT lst;
+ cout << "Test PutItemH and Print" << endl;
+ cout << "Correct if output is 4, 3, 2, 1, 0 on subsequent lines" << endl;
+ for (int i = 0; i < 5; i++) 
+   lst.PutItemH(i);
+ lst.Print();
+
+ cout << endl;
+ return 0;
+}
